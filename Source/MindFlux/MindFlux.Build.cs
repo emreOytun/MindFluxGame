bfee72c9ecffcdc33a52b405df5f3491aba4f2ad
emreOytun <emreoytun58@gmail.com>
2024-05-24 16:25:11 +0300

    changes for mobile package and elif's new pc, hud removal from mainlevel

--- Source/MindFlux/MindFlux.Build.cs
+++ Source/MindFlux/MindFlux.Build.cs
@@ -10,7 +10,7 @@ public class MindFlux : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-        PrivateDependencyModuleNames.AddRange(new string[] { "OnlineSubsystem", "OnlineSubsystemNull", "OnlineSubsystemSteam" });
+        PrivateDependencyModuleNames.AddRange(new string[] { "OnlineSubsystem"});
 
         // Uncomment if you are using Slate UI
         // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
